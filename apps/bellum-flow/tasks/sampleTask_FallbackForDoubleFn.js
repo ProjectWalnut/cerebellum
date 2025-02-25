9cdb453bb8fe5874062211fc372d06a9d9e1ecdb
Prathamesh Joshi <prathamesh.98223@gmail.com>
2025-02-25 03:57:05 +0000

    added input and output schemas and validators for tasks and jobs

--- apps/bellum-flow/tasks/sampleTask_FallbackForDoubleFn.js
+++ apps/bellum-flow/tasks/sampleTask_FallbackForDoubleFn.js
@@ -1,19 +1,36 @@
 const doubleFallback = async (input, error) => {
-    console.log(`Fallback for doubleTaskFn triggered due to error: ${error.message}`);
-  
-    // As a simple fallback, return the input unchanged.
-    return input;
-  };
+  let output = {};
+  console.log(`Fallback for doubleTaskFn triggered due to error: ${error.message}`);
 
+  // As a simple fallback, return the input unchanged.
+  output.number = input.number;
+  return output;
+};
 
-  module.exports = doubleFallback;
-  // This should be the name you add to taskEnums.js on the right side i.e. as value of the Key
-  module.exports.task_name = "FALLBACK_FOR_DOUBLE";
+// Define optional input schema
+doubleFallback.inputSchema = {
+  required: ['number'],
+  properties: {
+    number: { type: 'number' }
+  }
+};
 
-  // * New developers: Each task file MUST export a property called `task_name`.
-  // * For example, in your task file:
-  // *
-  // *    module.exports.task_name = "INCREMENT";
-  // *    module.exports = function(input) { ... };
-  // *
-  // * If a task file doesn't export `task_name`, an error is thrown.
+// Define optional output schema
+doubleFallback.outputSchema = {
+  required: ['number'],
+  properties: {
+    number: { type: 'number' }
+  }
+};
+
+module.exports = doubleFallback;
+// This should be the name you add to taskEnums.js on the right side i.e. as value of the Key
+module.exports.task_name = "FALLBACK_FOR_DOUBLE";
+
+// * New developers: Each task file MUST export a property called `task_name`.
+// * For example, in your task file:
+// *
+// *    module.exports.task_name = "INCREMENT";
+// *    module.exports = function(input) { ... };
+// *
+// * If a task file doesn't export `task_name`, an error is thrown.
--- apps/bellum-flow/tasks/sampleTask_IncrementFn.js
+++ apps/bellum-flow/tasks/sampleTask_IncrementFn.js
@@ -1,16 +1,34 @@
 // Sample task functions
 const incrementTaskFn = async (input) => {
-    return input + 1;
-  };
+  let output = {};
+  output.number = input.number + 1;
+  return output;
+};
 
-  module.exports = incrementTaskFn;
-  // This should be the name you add to taskEnums.js on the right side i.e. as value of the Key
-  module.exports.task_name = "INCREMENT";
+// Define optional input schema
+incrementTaskFn.inputSchema = {
+  required: ['number'],
+  properties: {
+    number: { type: 'number' }
+  }
+};
 
-  // * New developers: Each task file MUST export a property called `task_name`.
-  // * For example, in your task file:
-  // *
-  // *    module.exports.task_name = "INCREMENT";
-  // *    module.exports = function(input) { ... };
-  // *
-  // * If a task file doesn't export `task_name`, an error is thrown.
+// Define optional output schema
+incrementTaskFn.outputSchema = {
+  required: ['number'],
+  properties: {
+    number: { type: 'number' }
+  }
+};
+
+module.exports = incrementTaskFn;
+// This should be the name you add to taskEnums.js on the right side i.e. as value of the Key
+module.exports.task_name = "INCREMENT";
+
+// * New developers: Each task file MUST export a property called `task_name`.
+// * For example, in your task file:
+// *
+// *    module.exports.task_name = "INCREMENT";
+// *    module.exports = function(input) { ... };
+// *
+// * If a task file doesn't export `task_name`, an error is thrown.
